e08a71e1e9807f79eea939fa1159bca379e93700
Eano Corpus <eanocorpus@gmail.com>
2022-08-18 20:16:18 -0400

    created small gui, needs to fix how it ends

--- myscripts.js
+++ myscripts.js
@@ -9,27 +9,72 @@ function game() {
         "Scissors"
     ];
 
-    for (let i = 0; i < 5; i++) {
-        let playerAnswer = prompt("Please enter Rock, Paper, or Scissors: ");
+    const btn = document.querySelectorAll('#btn');
 
-        playerAnswer = playerAnswer.toLowerCase();
-        playerAnswer = playerAnswer.charAt(0).toUpperCase() + playerAnswer.slice(1);
+    // we use the .forEach method to iterate through each button
+    btn.forEach((btn) => {
 
-        let num1 = Math.floor(Math.random() * 3);
-
-        computerAnswer = getComputerChoice(myArray, num1);
-        let str1 = battle(playerAnswer, computerAnswer);
-
-        if (str1.includes("Win")) {
-            playerScore++;
-        } else if (str1.includes("Lose")) {
-            computerScore++;
-        }
-
-        console.log("Score - User: " + playerScore + " Computer Score: " + computerScore);
-    }
+        // and for each one we add a 'click' listener
+        btn.addEventListener('click', () => {
 
 
+            let playerAnswer = btn.innerText;
+
+            playerAnswer = playerAnswer.toLowerCase();
+            playerAnswer = playerAnswer.charAt(0).toUpperCase() + playerAnswer.slice(1);
+
+            let num1 = Math.floor(Math.random() * 3);
+
+            computerAnswer = getComputerChoice(myArray, num1);
+            let str1 = battle(playerAnswer, computerAnswer);
+
+            if (str1.includes("Win")) {
+                playerScore++;
+            } else if (str1.includes("Lose")) {
+                computerScore++;
+            }
+
+
+            //  let text = "You Selected: " + playerAnswer + " Computer Selected: " + computerAnswer + "" <br> "Score - User: " + playerScore + " Computer Score: " + computerScore;;
+            const results = document.querySelector('#results');
+
+            results.textContent = "";
+
+            const content1 = document.createElement('div');
+            content1.classList.add('content1');
+            content1.textContent = "You Selected: " + playerAnswer + " | Computer Selected: " + computerAnswer;
+            results.appendChild(content1);
+
+
+            const content2 = document.createElement('div');
+            content2.classList.add('content2');
+            content2.textContent = String(str1);
+            results.appendChild(content2);
+
+
+            const content3 = document.createElement('div');
+            content3.classList.add('content3');
+            content3.textContent = "Score - User: " + playerScore + " Computer Score: " + computerScore;
+            results.appendChild(content3);
+
+            if (playerScore === 5) {
+                const content4 = document.createElement('div');
+                content4.classList.add('content3');
+                content4.textContent = "Player Wins!"
+                results.appendChild(content4);
+
+                return;
+            } else if (computerScore === 5) {
+                const content4 = document.createElement('div');
+                content4.classList.add('content3');
+                content4.textContent = "Computer Wins!";
+                results.appendChild(content4);
+
+                return;
+            }
+
+        });
+    });
 }
 
 function getComputerChoice(myArray, num) {
